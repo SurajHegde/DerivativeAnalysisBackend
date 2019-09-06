95dcb0661953c0ceb08aa4f419380c700bcedb75
Utkarsh <utkarsh@gmail.com>
2019-09-06 15:56:42 +0530

    sysout added

--- src/com/controller/LoginServlet.java
+++ src/com/controller/LoginServlet.java
@@ -33,6 +33,7 @@ public class LoginServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
+		System.out.println("hello");
 		
 	}
 
