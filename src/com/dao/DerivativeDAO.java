edbb696d4867bc9b4336ab8299a4cfb0be6c786d
Joe Santhosh Nangani <joesantosh2210@gmail.com>
2019-09-08 15:33:19 +0530

    Generated payoff coordinates

--- src/com/dao/DerivativeDAO.java
+++ src/com/dao/DerivativeDAO.java
@@ -1,5 +1,9 @@
 package com.dao;
 
+import com.logic.Pair;
+import com.pojo.Holding;
+import java.util.List;
+
 public interface DerivativeDAO {
-	
+	public List<Pair> generatePayoff(List<Holding> holdingList);
 }
