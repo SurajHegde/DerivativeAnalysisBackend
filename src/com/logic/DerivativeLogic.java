79ea3ca7458ffec51da77878c8f1d96c0cb65839
Joe Santhosh Nangani <joesantosh2210@gmail.com>
2019-09-08 18:29:27 +0530

    Maximum profit and loss functions have been added

--- src/com/logic/DerivativeLogic.java
+++ src/com/logic/DerivativeLogic.java
@@ -2,19 +2,20 @@ package com.logic;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 import com.pojo.Holding;
 
 public class DerivativeLogic {
 
-	private double calcMaxProfit() {
-		return 0;
+	public double calcMaxProfit(List<Pair> coordinateList) {
+		return Collections.max(coordinateList,Comparator.comparing(coord -> coord.getY())).getY();
 	}
-	private double calcMaxLoss() {
-		return 0;
+	public double calcMaxLoss(List<Pair> coordinateList) {
+		return Collections.min(coordinateList,Comparator.comparing(coord -> coord.getY())).getY();
 	}
-	private List<Double> calcBreakeven(List<Pair> coordinateList) {
+	public List<Double> calcBreakeven(List<Pair> coordinateList) {
 		List<Double> breakEvenPoints = new ArrayList<>();
 		for (int i=0;i<(coordinateList.size()-1);i++) {
 			double distance = (coordinateList.get(i+1).getX()-coordinateList.get(i).getX())*Math.abs(coordinateList.get(i).getY()/(coordinateList.get(i+1).getY()-coordinateList.get(i).getY()));
@@ -22,7 +23,7 @@ public class DerivativeLogic {
 		}
 		return breakEvenPoints;
 	}
-	private double calcFairPrice() {
+	public double calcFairPrice() {
 		return 0;
 	}
 	public List<Pair> generatePayoff(List<Holding> holdingList) {
