f314147c255664d55e4db774e9d2ba98aba34f87
Joe Santhosh Nangani <joesantosh2210@gmail.com>
2019-09-09 16:24:57 +0530

    Adding user create account and login service

---
+++ src/com/controller/login/UserAuthCreateAccount.java
@@ -0,0 +1,84 @@
+package com.controller.login;
+
+import org.json.simple.JSONObject;
+
+import java.util.List;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import com.dao.UserDAOImpl;
+import com.pojo.Holding;
+
+@Path("/UserAuth")
+public class UserAuthCreateAccount {
+	
+	@POST
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	
+	@Path("/CreateAccount")
+	public JSONObject createAccount(JSONObject incomingData) {
+		String message;
+		String url;
+		List <Holding> userHoldings = null;
+		String emailId = (String) incomingData.get("emailId");
+		String firstName = (String) incomingData.get("firstName");
+		String lastName = (String) incomingData.get("lastName");
+		String password = (String) incomingData.get("password");
+		String confirmPassword = (String) incomingData.get("confirmPassword");
+		
+		UserDAOImpl userDao = new UserDAOImpl();
+		String output = userDao.createAccount(emailId, firstName, lastName, password, confirmPassword);
+		
+		if(!output.equals("User was registered successfully\n")) {
+			message = output;
+			url = "/login";
+		} else {
+			message = "User was registered successfully";
+			url = "/app/dashboard";
+		}
+		
+		JSONObject response = new JSONObject();
+		response.put("message", message);
+		response.put("url", url);
+		response.put("uniqueIdentifier", emailId);
+		response.put("name", firstName+lastName);
+		response.put("userHolding", userHoldings);
+		
+		return(response);
+	}
+	
+	@Path("/LoginCheck")
+	public JSONObject login(JSONObject incomingData) {
+		String message;
+		String url;
+		List <Holding> userHoldings = null;
+		String emailId = (String) incomingData.get("emailId");
+		String password = (String) incomingData.get("password");
+		
+		UserDAOImpl userDao = new UserDAOImpl();
+		String output = userDao.login(emailId, password);
+		
+		if(output.equals("Invalid credentials") || output.equals("No account with such an email exists")) {
+			message = output;
+			url = "/login";
+		} else {
+			message = "Login Successful";
+			url = "/app/dashboard";
+			userHoldings = userDao.getAllHoldings(emailId);
+		}
+		
+		JSONObject response = new JSONObject();
+		response.put("message", message);
+		response.put("url", url);
+		response.put("uniqueIdentifier", emailId);
+		response.put("name", output);
+		response.put("userHolding", userHoldings);
+		
+		return(response);
+	}
+}
--- src/com/controller/login/UserAuthLogin.java
+++ src/com/controller/login/UserAuthLogin.java
@@ -1,7 +1,5 @@
 package com.controller.login;
 
-import org.json.simple.JSONObject;
-
 import java.util.List;
 
 import javax.ws.rs.Consumes;
@@ -10,49 +8,18 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.json.simple.JSONObject;
+
 import com.dao.UserDAOImpl;
 import com.pojo.Holding;
 
-@Path("/UserAuth")
-public class UserAuth {
+@Path("/LoginCheck")
+public class UserAuthLogin {
 	
 	@POST
 	@Produces(MediaType.APPLICATION_JSON)
 	@Consumes(MediaType.APPLICATION_JSON)
 	
-	@Path("/CreateAccount")
-	public JSONObject createAccount(JSONObject incomingData) {
-		String message;
-		String url;
-		List <Holding> userHoldings = null;
-		String emailId = (String) incomingData.get("emailId");
-		String firstName = (String) incomingData.get("firstName");
-		String lastName = (String) incomingData.get("lastName");
-		String password = (String) incomingData.get("password");
-		String confirmPassword = (String) incomingData.get("confirmPassword");
-		
-		UserDAOImpl userDao = new UserDAOImpl();
-		String output = userDao.createAccount(emailId, firstName, lastName, password, confirmPassword);
-		
-		if(!output.equals("User was registered successfully\n")) {
-			message = output;
-			url = "/login";
-		} else {
-			message = "User was registered successfully";
-			url = "/app/dashboard";
-		}
-		
-		JSONObject response = new JSONObject();
-		response.put("message", message);
-		response.put("url", url);
-		response.put("uniqueIdentifier", emailId);
-		response.put("name", firstName+lastName);
-		response.put("userHolding", userHoldings);
-		
-		return(response);
-	}
-	
-	@Path("/LoginCheck")
 	public JSONObject login(JSONObject incomingData) {
 		String message;
 		String url;
