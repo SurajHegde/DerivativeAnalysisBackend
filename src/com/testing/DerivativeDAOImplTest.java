4589a6192ab233456d1d270e1be189d7840d76f2
Utkarsh <utkarshsharmaas@gmail.com>
2019-09-10 19:14:38 +0530

    Test for DerivativeDAO

---
+++ src/com/testing/DerivativeDAOImplTest.java
@@ -0,0 +1,51 @@
+package com.testing;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import com.dao.DerivativeDAOImpl;
+
+class DerivativeDAOImplTest {
+
+	@Test
+	void testCheckUserHolding() {
+		//fail("Not yet implemented");
+	
+	}
+
+	@Test
+	void testAddUserHolding_new() {
+		DerivativeDAOImpl daoImpl= new DerivativeDAOImpl();
+		//boolean actual= daoImpl.addUserHolding("utkarshsharmaas@gmail.com","CE","LONG",2150,"RELIANCE","29-OCT-19",200,11.4,6.55,5,105.25);
+		boolean actual= daoImpl.addUserHolding("shantanu.vijay8@gmail.com", "PE","LONG",2100, "HDFCBANK", "02-NOV-19",200,11.4, 7.25,4, 100.25);
+		assertEquals(true, actual);
+	}
+	
+	@Test
+	void testAddUserHolding_existing() {
+		DerivativeDAOImpl daoImpl= new DerivativeDAOImpl();
+		boolean actual= daoImpl.addUserHolding("utkarshsharmaas@gmail.com","FUT","LONG",2000,"HDFCBANK","02-NOV-19",200,11.4,6.55,5,105.75);
+		//boolean actual= daoImpl.addUserHolding("sailimaye@gmail.com", "CE","SHORT",2000, "HDFCBANK", "02-NOV-19",200,11.4, 7.25,4, 100.25);
+		assertEquals(true, actual);
+	}
+	@Test
+	void testAddUserHolding_existing1() {
+		DerivativeDAOImpl daoImpl= new DerivativeDAOImpl();
+		boolean actual= daoImpl.addUserHolding("utkarshsharmaas@gmail.com","FUT","LONG",2000,"HDFCBANK","02-NOV-19",200,11.4,6.55,4,105.25);
+		
+		//boolean actual= daoImpl.addUserHolding("sailimaye@gmail.com", "CE","SHORT",2000, "HDFCBANK", "02-NOV-19",200,11.4, 7.25,4, 100.25);
+		assertEquals(true, actual);
+	}
+	@Test
+	void testAddUserHolding_existing2() {
+		DerivativeDAOImpl daoImpl= new DerivativeDAOImpl();
+		boolean actual= daoImpl.addUserHolding("utkarshsharmaas@gmail.com","CE","SHORT",2000,"HDFCBANK","02-NOV-19",200,11.4,6.55,4,105.25);
+		
+		//boolean actual= daoImpl.addUserHolding("sailimaye@gmail.com", "CE","SHORT",2000, "HDFCBANK", "02-NOV-19",200,11.4, 7.25,4, 100.25);
+		assertEquals(true, actual);
+	}
+
+
+
+}
